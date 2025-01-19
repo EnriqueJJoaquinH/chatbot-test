3a4a28440ba42ba43fb9803332f42042d0172e2c
enriquejh <enriquejjoaquinh@gmail.com>
2025-01-18 21:48:21 -0600

    Add sending message functionality with Twilio
    Twilio library allows us to use the Twilio API to send messages with
    WhatsApp. A .env file was added to keep the credentials and sensible
    data isolated from our code. This file will not be pushed into the
    repository

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-""" Setting the workspace """
+""" Sending message without template """
 
-print('Hello World!')
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+twilio_num = os.getenv('TWILIO_NUM')
+client_num = os.getenv('CLIENT_NUM')
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_ = f'whatsapp:{twilio_num}',
+    body = 'Good morning',
+    to = f'whatsapp:{client_num}'
+)
+
+print(message.sid)
